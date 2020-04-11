8e920d82951d7c702d1c6a67d851d64127df2ac9
KuTin98 <hoangtanphatqn@gmail.com>
2020-04-11 12:52:55 +0700

    up code all

---
+++ app/src/main/java/com/edu/news/Common/OnBoardingActivity.java
@@ -0,0 +1,16 @@
+package com.edu.news.Common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.edu.news.R;
+
+public class OnBoardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+    }
+}
---
+++ app/src/main/java/com/edu/news/User/AllCategories.java
@@ -0,0 +1,31 @@
+package com.edu.news.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.edu.news.R;
+
+public class AllCategories extends AppCompatActivity {
+
+    ImageView backBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_categories);
+
+        //Hook
+        backBtn = findViewById(R.id.back_pressed);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AllCategories.super.onBackPressed();
+
+            }
+        });
+
+    }
+}
